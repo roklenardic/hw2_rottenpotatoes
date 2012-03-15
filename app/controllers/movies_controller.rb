aa6534eda50cf73d7ffc2d48c05bffd3b0ada27b
Ubuntu <ubuntu@ip-10-235-38-108.eu-west-1.compute.internal>
2012-03-15 09:14:38 +0000

    movies sorting and header highlight

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,12 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    if params[:sort_by]
+      @sort_by = params[:sort_by]
+      @movies = Movie.all(:order => @sort_by)
+    else
+      @movies = Movie.all
+    end
   end
 
   def new
