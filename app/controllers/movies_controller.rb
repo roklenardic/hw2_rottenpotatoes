b303ab36e674d8654e96b859805a1802a5a72040
Ubuntu <ubuntu@ip-10-235-38-108.eu-west-1.compute.internal>
2012-03-19 20:16:24 +0000

    hw2 part 5

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,10 +15,11 @@ class MoviesController < ApplicationController
     if params[:sort_by]
       @sort_by = params[:sort_by]
       session[:sort_by] = @sort_by
+      redirect_params += 'sort_by=' + @sort_by
     elsif session[:sort_by]
       @sort_by = session[:sort_by]
+      redirect_params += 'sort_by=' + @sort_by
     end
-    redirect_params += 'sort_by=' + @sort_by
 
     if params[:commit] == "Refresh"
       if params[:ratings]
